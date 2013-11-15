22d7529bd2eb63840902d4f64493f9fbe8afe5fb
Dominik <Dominik@Dominik-PC>
2013-11-15 17:42:49 +0100

    added index note list and delete button

--- notizverwaltung/app/controllers/notes_controller.rb
+++ notizverwaltung/app/controllers/notes_controller.rb
@@ -8,13 +8,22 @@ class NotesController < ApplicationController
 	end
 
 	def create
+		#render message: params[:note].inspect
 		@note = Note.new(note_params)
 		@note.save
-		redirect_to @note
+		redirect_to notes_path
+		#redirect_to @note
+		#redirect_to action: :show, id:@note.id
 	end
 
 	def show
 		@note = Note.find(params[:id])
+	end
+
+	def destroy
+		@note = Note.find(params[:id])
+		@note.destroy
+		redirect_to notes_path
 	end
 
 	private
